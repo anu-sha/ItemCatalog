a81ff2b683e185049a798dfb542c53dc52ae6864
anu-sha <anusha.mogallapu@gmail.com>
2016-05-20 14:12:23 -0500

    Add files via upload

---
+++ database_setup.py
@@ -0,0 +1,65 @@
+import os
+import sys
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base=declarative_base()
+
+#User class for maintaining users data if we need to extend the app to allow only owners edit/delete items
+class User(Base):
+    __tablename__='user'
+    id=Column(Integer, primary_key=True)
+    name=Column(String(250), nullable=False)
+    picture=Column(String(250), nullable=False)
+    email=Column(String(250))
+
+#Category class ->table to hold category information
+class Category(Base):
+    __tablename__='category'
+    name=Column(String(80),nullable=False)
+    id=Column(Integer, primary_key=True)
+    user_id=Column(Integer, ForeignKey('user.id'))
+    user=relationship(User)
+    #for providing json endpoint
+    @property
+    def serialize(self):
+
+        return {
+            'name': self.name,
+            'id': self.id,
+            
+        }
+    
+#Item class ->table holds all items along with their categories
+class Item(Base):
+    __tablename__='item'
+    name=Column(String(80),nullable=False)
+    id=Column(Integer,primary_key=True)
+    course=Column(String(250))
+    description=Column(String(250))
+    category_id=Column(Integer, ForeignKey('category.id'))
+    category=relationship(Category)
+    user_id=Column(Integer, ForeignKey('user.id'))
+    user=relationship(User)
+
+    #for providing json endpoint
+    @property
+    def serialize(self):
+
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            
+        }
+
+
+
+#create the database named itemcatalog    
+engine=create_engine('sqlite:///itemcatalog.db')
+
+Base.metadata.create_all(engine)    
+    
